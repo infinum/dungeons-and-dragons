0efb7084bafdec609c2cefe4a6631ec4f055ed55
Darko Kukovec <darko@kukovec.eu>
2017-03-04 23:51:04 +0100

    Tweak the character name input size

--- app/components/Character/Header/Header.tsx
+++ app/components/Character/Header/Header.tsx
@@ -1,5 +1,4 @@
 import * as classnames from 'classnames';
-import {observable} from 'mobx';
 import {observer} from 'mobx-react';
 import * as React from 'react';
 
@@ -8,42 +7,30 @@ import {Character} from 'stores/models';
 
 import * as styles from './Header.scss';
 
-@observer
-export class Header extends React.Component<{
+export const Header = observer(({
+  character,
+  className,
+}: {
   character: Character,
   className?: string,
-}, {}> {
-  public state = observable.object({
-    widthEl: null,
-  });
+}) => (
+  <div className={classnames(className, styles.header)}>
+    <div className={styles.nameContainer}>
+      <input
+        type='text'
+        placeholder='Character name'
+        value={character.name}
+        onChange={(e) => character.assign('name', e.target.value)}
+        className={styles.characterName}
+      />
 
-  public render() {
-    const {character, className} = this.props;
-
-    return (
-      <div className={classnames(className, styles.header)}>
-        <input
-          type='text'
-          placeholder='Character name'
-          value={character.name}
-          onChange={(e) => character.assign('name', e.target.value)}
-          className={styles.characterName}
-          style={this.state.widthEl ? {
-            width: this.state.widthEl.getBoundingClientRect().width,
-          } : null}
-        />
-
-        <div
-          ref={(el) => this.state.widthEl = el}
-          className={styles.widthCheck}
-        >
-          {character.name || 'Character name'}
-        </div>
-
-        <span className={styles.level}>
-          Level {character.level && character.level.toString()}
-        </span>
+      <div className={styles.widthCheck}>
+        {character.name || 'Character name'}
       </div>
-    );
-  }
-}
+    </div>
+
+    <span className={styles.level}>
+      Level {character.level && character.level.toString()}
+    </span>
+  </div>
+));
